c0b3590532476d5bb55f6c4d8a9a82c778d0c5b2
Justin Nguyen <justinhnguyen88@gmail.com>
2019-09-24 15:46:45 -0700

    loading spinner created

--- src/index.js
+++ src/index.js
@@ -1,27 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+  state = { lat: null, errorMessage: '' };
 
-        this.state = { lat: null };
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  // Helper Function
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+        return <div>Error: {this.state.errorMessage}</div>;
+    }
+  
+    if (!this.state.errorMessage && this.state.lat) {
+        return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => {console.log(position)},
-            (err) => {console.log(err) }
-        );
+    return <Spinner message="Please accept location request"/>;
+  }
 
-        return <div>Latitude: </div>;
-    }
+  // Render
+  render() {
+    return (
+        <div className="border red">
+            {this.renderContent()}
+        </div>
+    )
+  }
 }
 
-
-
-
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-)
+ReactDOM.render(<App />, document.querySelector('#root'));
